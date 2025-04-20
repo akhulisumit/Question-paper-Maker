e506d03d54305eec1dbd652684c819ff843931c0
akhulisumit <sumitakhuli5022@gmail.com>
2025-04-21 02:04:22 +0530

    Edited Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 function Navbar() {
-  const navItems = ["Home", "About", "Services", "Pricing", "Contact"];
+  const navItems = ["Home", "About", "Pricing", "Contact"];
   const [active, setActive] = useState(null);
 
   return (
@@ -9,12 +9,12 @@ function Navbar() {
       <div className="max-w-screen-xl flex items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3">
           <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8"
+            src="/logo.png"
+            className="h-12"
             alt="Flowbite Logo"
           />
           <span className="text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
+            Question Paper Maker
           </span>
         </a>
 
