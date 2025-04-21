e719f7cdb2e3e38e8531e7e3864a308aa3abf99f
akhulisumit <sumitakhuli5022@gmail.com>
2025-04-21 20:45:54 +0530

    deploy 3

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from "../components/hero"
+import Hero from "../components/Hero"
 import FormInput from '../components/QuestionPaperForm'
 function Home() {
   return (
